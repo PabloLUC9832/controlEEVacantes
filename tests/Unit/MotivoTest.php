c9cb7f36ab4aa6e2ce677f1b5570f5fabd95b867
PabloLUC9832 <lucaspablo340@outlook.com>
2022-10-21 12:58:14 -0500

    test prueba

--- tests/Unit/MotivoTest.php
+++ tests/Unit/MotivoTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\Models\Motivo;
+use Illuminate\Http\UploadedFile;
 use PHPUnit\Framework\TestCase;
 
 class MotivoTest extends TestCase
@@ -11,13 +13,21 @@ class MotivoTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+/*    public function test_example()
     {
         $this->assertTrue(true);
-    }
+    }*/
 
-    public function test_subir_CSV()
+    public function test_subir_csv()
     {
+        //Storage::fake('motivos');
+        $file = UploadedFile::fake()->create('motivo.csv');
+        $response = $this->post('/import',[
+            'file' => $file,
+        ]);
+
+        //$motivo = Motivo::importToDB();
+        $this->post('import',[$file])->assertRedirect('import');
 
     }
 
