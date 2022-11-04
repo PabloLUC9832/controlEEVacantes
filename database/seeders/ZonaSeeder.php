421b110600ef825730086a926381859e9e13831e
PabloLUC9832 <lucaspablo340@outlook.com>
2022-11-04 12:49:13 -0600

    Creación delo modelo,seeder,migrationc,controller de Zona, se lleno el seeder

---
+++ database/seeders/ZonaSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Zona;
+
+class ZonaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $zona1 = new Zona();
+        $zona1->nombre = "Xalapa";
+        $zona1->save();
+
+        $zona2 = new Zona();
+        $zona2->nombre = "Veracruz";
+        $zona2->save();
+
+        $zona3 = new Zona();
+        $zona3->nombre = "Orizaba Cordoba";
+        $zona3->save();
+
+        $zona4 = new Zona();
+        $zona4->nombre = "Poza rica Tuxpan";
+        $zona4->save();
+
+        $zona5 = new Zona();
+        $zona5->nombre = "Coatzacoalcos Minatitlan";
+        $zona5->save();
+
+    }
+}
--- resources/views/vacante/index.blade.php
+++ resources/views/vacante/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Registrar EE Vacante</title>
+    <title>Lista de EE vacantes</title>
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
     <!-- Scripts -->
