09951b4d3993ccba09da522563d2fec9e3764b61
PabloLUC9832 <lucaspablo340@outlook.com>
2023-01-19 19:33:01 -0600

    no vacantes, vacantes, todas para ambos roles, busqueda funcionando

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,6 @@ Route::controller(VacanteController::class)->group(function (){
         Route::get('/vacante/import',  'import')->name('import');
         Route::post('/vacante/upload','uploadCSV')->name('upload');
 
-        Route::get('/vacante/category',  'category')->name('category');
     });
 
 });
