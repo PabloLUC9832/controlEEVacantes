773fa3d1d6b728b056d2171f1e8bdb95ca593a27
DGAAEA-PROJECT <dgaaeauv.proyecto@gmail.com>
2023-02-15 17:57:04 -0600

    fix

--- resources/views/zonaDependencia/selectIdZonaEdit.blade.php
+++ resources/views/zonaDependencia/selectIdZonaEdit.blade.php
@@ -1,7 +1,7 @@
 <div class="col-span-6">
     <label for="idZona-dropdown" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">N° de la zona</label>
     <select  id="idZona-dropdown" name="id_zona" class="estiloSelect">
-        <option value="{{$dependencia->id_zona}}~{{$dependencia->nombre_dependencia}}">{{$dependencia->id_zona}}~{{$nombreZona}}</option>
+        <option value="{{$dependencia->id_zona}}~{{$dependencia->nombre_zona}}">{{$dependencia->id_zona}}~{{$nombreZona}}</option>
         @foreach ($zonas as $data)
             <option value="{{$data->id}}~{{$data->nombre}}">
                 {{$data->id}}~{{$data->nombre}}
