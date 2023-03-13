d6bfbf3ebf9ce8de6bd50dc34738bb81fe1db774
DGAAEA-PROJECT <dgaaeauv.proyecto@gmail.com>
2023-03-13 14:18:58 -0600

    me olvide las migraciones y modelo

---
+++ database/migrations/2023_03_13_134805_create_historico_docentes.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historico_docentes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('vacanteID');
+            $table->integer('nPersonal')->nullable();
+            $table->text('nombreDocente');
+            $table->text('fechaAviso')->nullable();
+            $table->text('fechaAsignacion')->nullable();
+            $table->text('fechaRenuncia')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historico_docentes');
+    }
+};
